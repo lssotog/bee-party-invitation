2d99c45975aad9ec16cc5590c73473eb82a100f7
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2025-06-28 11:30:31 -0500

    first part

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import './style.css'
 
-const app = document.querySelector('#app')
+const app = document.querySelector("#app");
+
+app.addEventListener("click", (event) => {
+  const { target } = event;
+  const rootScene = document.querySelector("[data-scene='notification']");
+  const sceneOne = document.querySelector("[data-scene='one']");
+
+  if (!target.dataset.scene && target.closest("[data-scene]")) {
+    if (target.nodeName === "BUTTON" || target.closest("button")) {
+      rootScene.classList.add("hidden");
+      sceneOne.classList.remove("!hidden");
+    }
+  }
+});
